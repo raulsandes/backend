fac9de9e1ee25f6a4ca30d5beb24d04849e4010b
Raul <raulherbert7@gmail.com>
2022-05-23 23:06:46 -0300

    mudança staff

--- routes/users.js
+++ routes/users.js
@@ -1,90 +1,142 @@
 'use strict';
 
-const _       = require('lodash');
-const db      = require('../db.js');
+const _ = require('lodash');
+const db = require('../db.js');
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-router.get('/', function(req, res, next){
+router.get('/', function (req, res, next) {
     db('auth.users')
         .select()
         .where(req.query || {})
-        .then ( out => res.status(200).jsonp(out) )
-        .catch( err => res.status(404).jsonp(err) );
+        .then(out => res.status(200).jsonp(out))
+        .catch(err => res.status(404).jsonp(err));
 });
 
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-router.post('/', function(req, res, next){
+router.post('/', function (req, res, next) {
     db('auth.users')
         .insert(req.body)
         .returning('*')
-        .then ( out => res.status(200).jsonp(out) )
-        .catch( err => res.status(404).jsonp(err) );
+        .then(out => res.status(200).jsonp(out))
+        .catch(err => res.status(404).jsonp(err));
 });
 
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-router.get('/:user_id', function(req, res, next){
+router.get('/:user_id', function (req, res, next) {
     db('auth.users')
         .first()
         .where(req.params)
-        .then ( user => {
-            db('auth.endpoints')
+        .then(user => {
+            const ep = db('auth.endpoints')
                 .select()
                 .where(req.params)
-                .then ( endpoints => {
+
+            const st = db('auth.staff')
+                .select()
+                .where(req.params)
+
+            Promise.all([ep, st]).
+                then(([endpoints, staff]) => {
                     user.endpoints = endpoints;
-                    res.status(200).jsonp(user)
+                    user.staff = staff;
+                    return res.status(200).jsonp(user)
                 })
-        })
-        .catch( err => res.status(404).jsonp(err) );
-});
+                .catch(err => res.status(404).jsonp(err));
+            })
+        .catch(err => res.status(400).jsonp(err));
+        });
 
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-router.delete('/:user_id', function(req, res, next){
-    db('auth.users')
-        .del()
-        .where(req.params)
-        .returning('*')
-        .then ( out => res.status(200).jsonp(out) )
-        .catch( err => res.status(404).jsonp(err) );
-});
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    router.delete('/:user_id', function (req, res, next) {
+        db('auth.users')
+            .del()
+            .where(req.params)
+            .returning('*')
+            .then(out => res.status(200).jsonp(out))
+            .catch(err => res.status(404).jsonp(err));
+    });
 
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-router.patch('/:user_id', function(req, res, next){
-    db('auth.users')
-        .update(req.body, { patch: true })
-        .where(req.params)
-        .returning('*')
-        .then ( out => res.status(200).jsonp(out) )
-        .catch( err => res.status(404).jsonp(err) );
-});
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    router.patch('/:user_id', function (req, res, next) {
+        db('auth.users')
+            .update(req.body, { patch: true })
+            .where(req.params)
+            .returning('*')
+            .then(out => res.status(200).jsonp(out))
+            .catch(err => res.status(404).jsonp(err));
+    });
 
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-router.post('/:user_id/endpoints', function(req, res, next){
-    db('auth.endpoints')
-        .insert(req.body)
-        .returning('*')
-        .then ( out => res.status(200).jsonp(out) )
-        .catch( err => res.status(404).jsonp(err) );
-});
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    router.post('/:user_id/endpoints', function (req, res, next) {
+        db('auth.endpoints')
+            .insert(req.body)
+            .returning('*')
+            .then(out => res.status(200).jsonp(out))
+            .catch(err => res.status(404).jsonp(err));
+    });
 
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-router.delete('/:user_id/endpoints/:endpoint_id', function(req, res, next){
-    db('auth.endpoints')
-        .del()
-        .where(req.params)
-        .returning('*')
-        .then ( out => res.status(200).jsonp(out) )
-        .catch( err => res.status(404).jsonp(err) );
-});
-module.exports = router;
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    router.delete('/:user_id/endpoints/:endpoint_id', function (req, res, next) {
+        db('auth.endpoints')
+            .del()
+            .where(req.params)
+            .returning('*')
+            .then(out => res.status(200).jsonp(out))
+            .catch(err => res.status(404).jsonp(err));
+    });
+
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    router.get('/:user_id/staff', function (req, res, next) {
+        db('auth.staff')
+            .select()
+            .where(req.params)
+            .then(out => res.status(200).jsonp(out))
+            .catch(err => res.status(404).jsonp(err));
+    });
+
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    router.post('/:user_id/staff', function (req, res, next) {
+        db('auth.staff')
+            .insert(req.body)
+            .returning('*')
+            .then(out => res.status(200).jsonp(out))
+            .catch(err => res.status(404).jsonp(err));
+    });
+
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    router.delete('/:user_id/staff/:staff_id', function (req, res, next) {
+        db('auth.staff')
+            .del()
+            .where(req.params)
+            .returning('*')
+            .then(out => res.status(200).jsonp(out))
+            .catch(err => res.status(404).jsonp(err));
+    });
+
+    ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    router.patch('/:user_id/staff/:staff_id', function (req, res, next) {
+        db('auth.staff')
+            .update(req.body, { patch: true })
+            .where(req.params)
+            .returning('*')
+            .then(out => res.status(200).jsonp(out))
+            .catch(err => res.status(404).jsonp(err));
+    });
+    module.exports = router;
